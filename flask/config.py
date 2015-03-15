416d493c17f93ff94b295c596063d88d726fda8f
Joe Kohlmann <kohlmannj@gmail.com>
2015-03-15 16:24:31 -0700

    * Created a config file for Heroku deployment.

---
+++ flask/config.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+__author__ = 'kohlmannj'
+
+import os
+from collections import OrderedDict
+
+class Config(object):
+    DEBUG = False
+    # NYT Article Search API Key
+    # Request a key at http://developer.nytimes.com
+    SEARCH_API_KEY = os.environ.get("SEARCH_API_KEY","")
+    CACHE_ROOT = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        "static",
+        "cache"
+    )
+    ARTICLE_SEARCH_ROOT = os.path.join(
+        CACHE_ROOT,
+        "ArticleSearch"
+    )
+    QUERY_CACHE_PATH = os.path.join(
+        CACHE_ROOT,
+        "queries.json"
+    )
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
